dcc7c84c43a20455f8a85d493e0160c384323c27
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2022-01-30 20:48:39 +0100

    separating data from logic

---
+++ script/data.js
@@ -0,0 +1,23 @@
+const data = [
+  { name: 'HOME', url: '../index.html' },
+  { name: 'AGENDA', url: 'agenda.html' },
+  { name: 'PRICING', url: 'pricing.html' },
+  { name: 'SCHEDULE', url: 'schedule.html' },
+  { name: 'MEDIA', url: 'media.html' },
+  {
+    name: 'FITDANCE',
+    url: '#',
+    dropdownData: [
+      { name: 'ABOUT', url: 'about.html' },
+      { name: 'CONTACT', url: 'contact.html' },
+      { name: 'REGISTER', url: 'register.html' },
+      { name: 'THE TEACHER', url: 'about.html' },
+    ],
+  },
+];
+const attributes = {
+  src: '../images/icons/logo1.svg',
+  width: '80',
+  height: '80',
+  alt: 'logo Brazilian Fitdance',
+};
